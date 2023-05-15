e1b0889e4c771680bfd5f10fd2fa6957827bf0ac
Oshurkov <oshurkov.oc@gmail.com>
2023-05-15 16:20:08 +0500

    test

---
+++ src/main/java/com/example/TelegramPoster/TelegramPosterApplication.java
@@ -0,0 +1,13 @@
+package com.example.TelegramPoster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramPosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramPosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TelegramPoster/TelegramPosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TelegramPoster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramPosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
